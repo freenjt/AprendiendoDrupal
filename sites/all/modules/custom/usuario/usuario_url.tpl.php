98dcd0bf80c223581583d51823d92d2f48ace9de
freenjt <freenjt@gmail.com>
2014-02-20 17:43:47 -0500

    creando y formateando campos y widgets

---
+++ sites/all/modules/custom/usuario/usuario_url.tpl.php
@@ -0,0 +1,10 @@
+<a href="<?php print $url; ?>"><?php print $title; ?></a>
+<div class="website-title"><?php print $url; ?></div>
+
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
