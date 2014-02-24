5544491d30a76958a2acaca2227aa0c149838955
freenjt <freenjt@gmail.com>
2014-02-24 15:24:41 -0500

    utilizando colorbox

---
+++ sites/all/modules/custom/ww_campos/agregar_colorbox.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function ($) {
+    
+    $(document).ready(function() {
+        $("#group1").colorbox({
+            rel:'group1'
+        });
+    });
+    
+    
+})(jQuery);
+
