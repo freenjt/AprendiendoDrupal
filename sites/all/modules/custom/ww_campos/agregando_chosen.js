2fc949f4c2ce195c3e290cb233095ff2962d75d4
freenjt <freenjt@gmail.com>
2014-02-25 14:31:32 -0500

    añadiendo chosen

---
+++ sites/all/modules/custom/ww_campos/agregando_chosen.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function ($) {
+    
+    /*var config = {
+      '.chosen-select'           : {},
+      '.form-select.chosen-select-deselect'  : {allow_single_deselect:true},
+      '.chosen-select-no-single' : {disable_search_threshold:10},
+      '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
+      '.chosen-select-width'     : {width:"95%"}
+    }
+    for (var selector in config) {
+      $(selector).chosen(config[selector]);
+    }*/
+ 
+$('.chosen-select-deselect').chosen();  
+    
+})(jQuery);
+
