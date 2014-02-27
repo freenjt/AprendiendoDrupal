c6dbe35d3a6e12ce83f049e48fc9f6c6e22bd7e5
freenjt <freenjt@gmail.com>
2014-02-27 16:11:39 -0500

    subiendo cambios

---
+++ sites/all/modules/custom/ww_persona/js/agregar_datepicker.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+(function ($) {
+
+  $('#edit-datepicker').datepicker({
+                dateFormat: "yy-mm-dd"
+            });
+})(jQuery);
